6b6b7711776e23c9e1ffeefdf6a199880ce52f1f
Ranju <ranjukr@thoughtworks.com>
2017-08-03 18:32:36 +0530

    ranju/Sayan: port changed

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var http = require("http");
 
 var router = require("./router");
 
-http.createServer(router).listen(3000);
+http.createServer(router).listen(4000);
 
-console.log("server is running at port 3000...");
+console.log("server is running at port 4000...");
