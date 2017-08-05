cb8d3b916daabaea38505af9705cd072e80ad010
Ranju <ranjukr@thoughtworks.com>
2017-08-05 13:28:55 +0530

    ranju: es6 convention followed

--- public/js/index.js
+++ public/js/index.js
@@ -1,31 +1,31 @@
-var add = function () {
-    var num1 = $("#num1").val();
-    var num2 = $("#num2").val();
-    $.post('add', {number1: num1, number2: num2}, function (data) {
+const add = () => {
+    const num1 = $("#num1").val();
+    const num2 = $("#num2").val();
+    $.post('add', {number1: num1, number2: num2}, (data) => {
         $('#result').text(data);
     })
 };
 
-var sub = function () {
-    var num1 = $("#num1").val();
-    var num2 = $("#num2").val();
-    $.post('sub', {number1: num1, number2: num2}, function (data) {
+const sub = () => {
+    const num1 = $("#num1").val();
+    const num2 = $("#num2").val();
+    $.post('sub', {number1: num1, number2: num2}, (data) => {
         $('#result').text(data);
     })
 };
 
-var mul = function () {
-    var num1 = $("#num1").val();
-    var num2 = $("#num2").val();
-    $.post('mul', {number1: num1, number2: num2}, function (data) {
+const mul = () => {
+    const num1 = $("#num1").val();
+    const num2 = $("#num2").val();
+    $.post('mul', {number1: num1, number2: num2}, (data) => {
         $('#result').text(data);
     })
 };
 
-var div = function () {
-    var num1 = $("#num1").val();
-    var num2 = $("#num2").val();
-    $.post('div', {number1: num1, number2: num2}, function (data) {
+const div = () => {
+    const num1 = $("#num1").val();
+    const num2 = $("#num2").val();
+    $.post('div', {number1: num1, number2: num2}, (data) => {
         $('#result').text(data);
     })
 };
--- router/router.js
+++ router/router.js
@@ -1,27 +1,31 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require( 'express');
+const bodyParser = require( 'body-parser');
 
-var app = express();
+const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
     res.redirect('/views/index.html');
 });
 
-app.post('/views/add',function (req, res) {
-    res.end((Number(req.body.number1) + Number(req.body.number2)).toString());
+app.post('/views/add', (req, res) => {
+    const result = Number(req.body.number1) + Number(req.body.number2);
+    res.end(result.toString());
 });
 
-app.post('/views/sub',function (req, res) {
-    res.end((Number(req.body.number1) - Number(req.body.number2)).toString());
+app.post('/views/sub', (req, res) => {
+    const result = Number(req.body.number1) - Number(req.body.number2);
+    res.end(result.toString());
 });
 
-app.post('/views/mul',function (req, res) {
-    res.end((Number(req.body.number1) * Number(req.body.number2)).toString());
+app.post('/views/mul', (req, res) => {
+    const result = Number(req.body.number1) * Number(req.body.number2);
+    res.end(result.toString());
 });
 
-app.post('/views/div',function (req, res) {
-    res.end((Number(req.body.number1) / Number(req.body.number2)).toString());
+app.post('/views/div', (req, res) => {
+    const result = Number(req.body.number1) / Number(req.body.number2);
+    res.end(result.toString());
 });
 
 app.use(express.static('./public'));
